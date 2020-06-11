69402acae98f4bd569d4d84b06c76a538768c880
Aaron Halfaker <ahalfaker@wikimedia.org>
2020-06-11 20:53:14 +0000

    Cleanup and fixes for running wikidata cli.

--- mwtext/mwtext.py
+++ mwtext/mwtext.py
@@ -5,6 +5,7 @@ router = mwcli.Router(
     "This script provides access to a set of utilities for text processing",
     {'preprocess_text': "Converts an XML dump to preprocessed plaintext. " +
                         "One line per chunk.",
+     'preprocess_wikidata': "Convert an XML dump to preprocessed plaintext",
      'learn_vectors':   "Learn a set of word vectors from preprocessed " +
                         "plaintext",
      'word2vec2gensim': "Converts word2vec format to gensim KeyedVector " +
